eefec1f12ba7387b49b315460f5b13933a1d446d
trojan782 <davidjacobs522@gmail.com>
2022-07-30 03:18:13 +0100

    working on ci

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=flask_app
+docker build --tag=flask-app
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker images
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 flask_app
+docker run -p 8000:80 flask-app
